fb5885fb363fc30508bc2ea5b3b7beb1a98b9ec1
Tifet <tifety@gmail.com>
2018-01-12 23:03:19 +0300

    changes add

--- tasks/tasks.cpp
+++ tasks/tasks.cpp
@@ -23,20 +23,39 @@ class TeamTasks {
         void AddNewTask (const string& person) {
         tracker[person][TaskStatus::NEW]++;
         }
-        tuple <TasksInfo, TasksInfo> PerformPersonTasks (const string& person, int task_count) {
+
+
+ /*       tuple <TasksInfo, TasksInfo> PerformPersonTasks (const string& person, int task_count) {
             TasksInfo changes;
-            for (auto item : tracker[person]) {
-                    changes[item.first] = item.second;
-                    }
+            TasksInfo update;
+            TasksInfo old;
+            changes[TaskStatus::NEW]=
         }
+        }
+
+   */
     private:
-        map <string, TasksInfo> tracker; 
+        map <string, TasksInfo> tracker;
 };
+
+int Changes (int number_tasks, int & to_update) {
+    int answer;
+    if (number_tasks > to_update) {
+        answer = to_update;
+        to_update = 0;
+        cout << "in func " << to_update << endl;
+    } else {
+        answer = number_tasks;
+        to_update -= number_tasks;
+        cout << "in func" <<  to_update << endl;
+    }
+    return answer;
+}
 
 void PrintTasksInfo (TasksInfo tasks_info) {
     cout << tasks_info[TaskStatus::NEW] << "new tasks, "
-         << tasks_info[TaskStatus::IN_PROGRESS] << "tasks in progress, " 
-         << tasks_info[TaskStatus::TESTING] << "tasks are being tested" 
+         << tasks_info[TaskStatus::IN_PROGRESS] << "tasks in progress, "
+         << tasks_info[TaskStatus::TESTING] << "tasks are being tested"
          << " and " << tasks_info[TaskStatus::DONE] << "tasks are done \n";
 }
 
@@ -45,11 +64,29 @@ TasksInfo tasks;
 tasks[TaskStatus::NEW]=1;
 tasks[TaskStatus::IN_PROGRESS]=2;
 tasks[TaskStatus::TESTING]=3;
-tasks[TaskStatus::DONE]=4;
+//tasks[TaskStatus::DONE]=4;
 PrintTasksInfo(tasks);
 TeamTasks track;
 track.AddNewTask("I");
 track.AddNewTask("I");
 PrintTasksInfo(track.GetPersonTasksInfo("I"));
-
+cout << tasks[TaskStatus::NEW] << endl;
+int a = 5;
+cout << Changes (3, a) << " "<< a << endl; 
+cout << a << endl;
+a = 3;
+cout << Changes (5, a) << endl;
+cout << a << endl;
+a = 0;
+cout << Changes (0, a) << endl;
+cout << a << endl;
+a = 8;
+cout << Changes (0, a) << endl;
+cout << a << endl;
+a =0 ;
+cout << Changes (7, a) << endl;
+cout << a << endl;
+a = 4;
+cout << Changes (4, a) << endl;
+cout << a << endl;
 }
